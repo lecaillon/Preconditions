773c69393065706c2d5153191993c463c329f12a
lecaillon <lecaillon@hotmail.com>
2016-11-25 23:32:27 +0100

    Add tests for the method : Check.NotNull()

--- test/Preconditions.Tests/CheckTest.cs
+++ test/Preconditions.Tests/CheckTest.cs
@@ -1,14 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace Preconditions.Tests
 {
     public class CheckTest
     {
-        public CheckTest()
+        #region Check.NotNull
+
+        [Fact(DisplayName = "When the ref is null throws ArgumentNullException")]
+        public void When_reference_is_null_Throws_ArgumentNullException()
         {
+            object reference = null;
+
+            Assert.Throws<ArgumentNullException>(nameof(reference), () => Check.NotNull(reference, nameof(reference)));
         }
+
+        [Fact(DisplayName = "When the ref is not null returns the ref")]
+        public void When_reference_is_not_null_Returns_reference()
+        {
+            object reference = new object();
+
+            Assert.Same(reference, Check.NotNull(reference, nameof(reference)));
+        }
+
+        #endregion
     }
 }
